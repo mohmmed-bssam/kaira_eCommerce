8c4079f2c4c50bc7680406aa624302693b87a441
mohmmed bssam <mohmmedbssam97@gmail.com>
2026-01-18 21:11:13 +0200

    download breeze library

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,6 +46,14 @@ return [
             'throw' => false,
             'report' => false,
         ],
+        'custom' => [
+            'driver' => 'local',
+            'root' => public_path(''),
+            'url' => rtrim(env('APP_URL'), '/').'/public',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
 
         's3' => [
             'driver' => 's3',
